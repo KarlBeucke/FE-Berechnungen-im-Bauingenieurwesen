19f39c607e341b714b7eb70808bade49d8ec1b3f
KarlBeucke <83509876+KarlBeucke@users.noreply.github.com>
2026-06-14 17:32:13 +0200

    Add files via upload

---
+++ Dateieingabe/ModelldatenEditieren.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace FE_Berechnungen.Dateieingabe;
+
+public partial class ModelldatenEditieren
+{
+    public ModelldatenEditieren()
+    {
+        InitializeComponent();
+        var openFileDialog = new OpenFileDialog { Filter = "Eingabedateien (*.inp)|*.inp" };
+        if (openFileDialog.ShowDialog() == true)
+            txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+    }
+
+    public ModelldatenEditieren(string path)
+    {
+        InitializeComponent();
+        txtEditor.Text = File.ReadAllText(path);
+    }
+
+    private void BtnOpenFileClick(object sender, RoutedEventArgs e)
+    {
+        var openFileDialog = new OpenFileDialog { Filter = "Eingabedateien (*.inp)|*.inp" };
+        if (openFileDialog.ShowDialog() == true)
+            txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+    }
+
+    private void BtnSaveFile_Click(object sender, RoutedEventArgs e)
+    {
+        var saveFileDialog = new SaveFileDialog { Filter = "Eingabedateien (*.inp)|*.inp" };
+        if (saveFileDialog.ShowDialog() == true)
+            File.WriteAllText(saveFileDialog.FileName, txtEditor.Text);
+    }
+}
---
+++ Dateieingabe/NeuerDateiname.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace FE_Berechnungen.Dateieingabe;
+
+public partial class NeuerDateiname
+{
+    public string DateiName;
+
+    public NeuerDateiname()
+    {
+        InitializeComponent();
+    }
+
+    private void BtnDialogOk_Click(object sender, RoutedEventArgs e)
+    {
+        DateiName = Dateiname.Text;
+        DialogResult = true;
+    }
+
+    private void BtnDialogCancel_Click(object sender, RoutedEventArgs e)
+    {
+        DialogResult = false;
+    }
+}
